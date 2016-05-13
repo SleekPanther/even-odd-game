5bfda87b7c407e7aa5aaa9eb5348126bae72eddf
SleekPanther <macrobyte2@gmail.com>
2016-05-13 11:53:12 -0400

    rearranged imports

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -1,33 +1,31 @@
-//Consolidate/organize imports & field variables
-import java.io.File;
+import javafx.application.Application;
+import javafx.animation.Timeline;			//Timeline animations
+import javafx.animation.KeyFrame;
+import javafx.util.Duration;
+import java.text.DecimalFormat;
+import javafx.event.ActionEvent;				//keyboard & events
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;						//gui stuff
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+import java.util.Random;						//actually creating random numbers
+import java.io.File;								//mostly for saving high scores
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.DosFileAttributes;
-import java.text.DecimalFormat;
-import java.util.Random;
 import java.util.Scanner;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import javafx.util.Duration;
 /**
  * This game replicates a simple app which displays a random number and the user must decide if it's even or odd.
  * A time limit is imposed, & they get bonus time if they get 10 correct in a row.
